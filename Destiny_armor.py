8bdc3b6face60f22d8741b2896092d05d34c69dd
John Ferrell <johnferrell13@gmail.com>
2023-07-26 03:27:41 -0400

    Destiny 2 armor calculator

--- Destiny_armor.py
+++ Destiny_armor.py
@@ -2,15 +2,15 @@ import math
 import os
 import csv
 
-HELMETS = [[20, 2, 12], [17, 8, 18], [11, 10, 13]]
-GLOVES  = [[8, 7, 8], [18, 11, 6], [17, 18, 11]]
-BOOTS   = [[14, 2, 18], [10, 11, 10], [6, 19, 10]]
+HELMETS = [[20, 2, 12], [17, 8, 18], [11, 10, 13], [7,2,17], [12, 8, 9], [14, 15, 10], [6, 16, 20], [9, 12, 13], [11, 2, 14], [10, 2, 11]]
+GLOVES  = [[8, 7, 8], [18, 11, 6], [17, 18, 11], [6, 10, 12], [7, 2, 13], [8, 14, 9], [2, 10, 6], [2, 20, 23]]
+BOOTS   = [[14, 2, 18], [10, 11, 10], [6, 19, 10], [6, 13, 7], [6, 6, 6], [15, 6, 26]]
 
 def add_boots_stats(helmet_item, gloves_item, total_stat_list):
     no_boots = len(BOOTS)
     helm_and_gloves = [helmet_item[i] + gloves_item[i] for i in range(len(helmet_item))]
     for item in range(no_boots):
-        print('For boots number {}: '.format(item))
+        print('For boots number {}: '.format(item+1))
         stat_total = [helm_and_gloves[j] + BOOTS[item][j] for j in range(len(helm_and_gloves))]
         print('Stat total is: {}'.format(stat_total))
         total_stat_list.append(stat_total)
@@ -19,7 +19,7 @@ def add_boots_stats(helmet_item, gloves_item, total_stat_list):
 def add_gloves_stats(helmet_item, total_stat_list):
     no_gloves = len(GLOVES)
     for item in range(no_gloves):
-        print('For gloves number {}: '.format(item))
+        print('For gloves number {}: '.format(item+1))
         total_stat_list = add_boots_stats(helmet_item, GLOVES[item], total_stat_list)
     return total_stat_list
 
